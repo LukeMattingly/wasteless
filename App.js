04c8fa3d83704ce67c5625d1beef3b8f0a9c5b5c
Kycool13 <mattingly.luke@yahoo.com>
2021-11-26 19:30:54 -0700

    initial commet

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
 
+//iOs View -> UI ViewMapped to native widgets
 export default function App() {
+  console.log(require('./assets/favicon.png'));
+
+  const handlePress = () => console.log("Text Pressed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>WasteLess </Text>
+      <TouchableHighlight onPress={() => console.log("Image Tapped")}>
+        <Image
+          source = {{
+            width: 200,
+            height: 300,
+            uri:"https://picsum.photos/200/300"}}
+          />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
